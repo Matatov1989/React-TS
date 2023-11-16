d4678575316ef374928d952f667f5102ee437abc
Yury <matatov1989@gmail.com>
2023-11-16 08:47:52 +0200

    working with function props

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import Todos from "./components/Todos";
-import './App.css';
+import "./App.css";
 import Todo from "./models/todo";
+import NewTodo from "./components/NewTodo";
 
 function App() {
-  const todos = [
-    new Todo('Learn React'),
-    new Todo('Learn TypeScript'),
-  ];
+  const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+
+  const addTodoHandler = (todoText: string) => {
+
+  };
 
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
