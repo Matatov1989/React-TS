4e70b2b65916db1e260fca9cfe5d552243564cda
Yury <matatov1989@gmail.com>
2023-11-16 09:03:55 +0200

    managing state & typescript & adding styling

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,4 +1,5 @@
 import React, { useRef } from "react";
+import classes from "./NewTodo.module.css";
 
 const NewTodo: React.FC<{onAddTodo: (text: string) => void }> = (props) => {
   const todoTextInputRef = useRef<HTMLInputElement>(null);
@@ -6,7 +7,7 @@ const NewTodo: React.FC<{onAddTodo: (text: string) => void }> = (props) => {
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
 
-    const enteredText = todoTextInputRef.current?.value;
+    const enteredText = todoTextInputRef.current!.value;
 
     if (enteredText?.trim().length === 0) {
       // error
@@ -17,7 +18,7 @@ const NewTodo: React.FC<{onAddTodo: (text: string) => void }> = (props) => {
   };
 
   return (
-    <form onSubmit={submitHandler}>
+    <form onSubmit={submitHandler} className={classes.form} >
       <label htmlFor="text">Todo Text</label>
       <input type="text" id="text" ref={todoTextInputRef} />
       <button>Add Todo</button>
